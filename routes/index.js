e54c4e50f14cf5d03b5dec67a33c8fc6ef1ae510
Jiayang Dong <dongjiayang123@hotmail.com>
2024-08-26 17:32:06 -0700

    added and formatted the article page template. Added dummy texts to the article pages.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,25 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'Confort Corner' });
+  res.render('about', { title: 'About This Website' });
+});
+
+router.get('/sleep-better', function (req, res, next) {
+  res.render('sleep_better', {
+    title: 'Sleep Better'
+  });
+});
+
+router.get('/manage-axiety', function (req, res, next) {
+  res.render('manage_axiety', {
+    title: 'Manage Axiety'
+  });
+});
+
+router.get('/cope-with-stress', function (req, res, next) {
+  res.render('cope_with_stress', {
+    title: 'Cope With Stress'
+  });
 });
 
 module.exports = router;
